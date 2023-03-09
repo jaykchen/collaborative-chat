0e164bd726bab790e1912d23688eae81f8189b07
Ji Chen <jaykchen@gmail.com>
2023-03-09 10:15:18 -0500

    back to load env

--- src/lib.rs
+++ src/lib.rs
@@ -4,36 +4,36 @@ use slack_flows::{listen_to_channel, send_message_to_channel};
 use std::env;
 #[no_mangle]
 pub fn run() {
-    // dotenv().ok();
-    // let TEAM_NAME: String = match env::var("TEAM_NAME") {
-    //     Err(_) => "ik8".to_string(),
-    //     Ok(name) => name,
-    // };
+    dotenv().ok();
+    let TEAM_NAME: String = match env::var("TEAM_NAME") {
+        Err(_) => "ik8".to_string(),
+        Ok(name) => name,
+    };
 
-    // let CHANNEL_NAME: String = match env::var("CHANNEL_NAME") {
-    //     Err(_) => "general".to_string(),
-    //     Ok(name) => name,
-    // };
-    // let OPENPI_KEY_NAME: String = match env::var("OPENPI_KEY_NAME") {
-    //     Err(_) => "jaykchen".to_string(),
-    //     Ok(name) => name,
-    // };
-    listen_to_channel("ik8", "general", |sm| {
-        // let CHAT_ID: String = match env::var("CHAT_ID") {
-        //     Err(_) => "secondstate-collaborative_chat".to_string(),
-        //     Ok(id) => id,
-        // };
+    let CHANNEL_NAME: String = match env::var("CHANNEL_NAME") {
+        Err(_) => "general".to_string(),
+        Ok(name) => name,
+    };
+    let OPENPI_KEY_NAME: String = match env::var("OPENPI_KEY_NAME") {
+        Err(_) => "jaykchen".to_string(),
+        Ok(name) => name,
+    };
+    listen_to_channel(&TEAM_NAME, &CHANNEL_NAME, |sm| {
+        let CHAT_ID: String = match env::var("CHAT_ID") {
+            Err(_) => "secondstate-collaborative_chat".to_string(),
+            Ok(id) => id,
+        };
 
-        let c = chat_completion("jaykchen", "secondstate-collaborative_chat", &sm.text);
+        let c = chat_completion(&OPENPI_KEY_NAME, &CHAT_ID, &sm.text);
         if let Some(c) = c {
             if c.new_conversation {
                 send_message_to_channel(
-                    "ik8",
-                    "general",
+                    &TEAM_NAME,
+                    &CHANNEL_NAME,
                     "Let's start a new conversation!".to_string(),
                 );
             }
-            send_message_to_channel("ik8", "general", c.choice);
+            send_message_to_channel(&TEAM_NAME, &CHANNEL_NAME, c.choice);
         }
     });
 }
